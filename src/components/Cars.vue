66b9077b8e75402fe1876c2adf1780574e1c52fc
diegooquendo <diego.oquendo1@gmail.com>
2020-10-04 05:39:36 -0500

    Backend done

--- src/components/Cars.vue
+++ src/components/Cars.vue
@@ -4,38 +4,27 @@
     <router-link to="/createcars" class="btn btn-primary"
       >Create Car</router-link
     >
-    <table class="table">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Car Plate</th>
-          <th scope="col">Car Model</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-        </tr>
-      </tbody>
-    </table>
     <amplify-connect :query="listCarsQuery">
       <template slot-scope="{ loading, data, errors }">
         <div v-if="loading">loading</div>
         <div v-if="errors.length > 0">Errors</div>
         <div v-else-if="data">
-          {{ data }}
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Car Plate</th>
+                <th scope="col">Car Model</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="car in data.listCars.items" :key="car.id">
+                <td>{{ car.name }}</td>
+                <td>{{ car.car_plate }}</td>
+                <td>{{ car.car_model }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </template>
     </amplify-connect>
@@ -49,6 +38,7 @@ const listCarsQuery = `
   query listCars {
   listCars {
     items{
+      id
       name
       car_plate
       car_model
--- src/components/Drivers.vue
+++ src/components/Drivers.vue
@@ -4,40 +4,25 @@
     <router-link to="/createdrivers" class="btn btn-primary" v-if="isAdmin"
       >Create Driver</router-link
     >
-    <br />
-    <br />
-    <table class="table">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Birdthday</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-        </tr>
-      </tbody>
-    </table>
     <amplify-connect :query="listDriversQuery">
       <template slot-scope="{ loading, data, errors }">
         <div v-if="loading">loading</div>
         <div v-if="errors.length > 0">Errors</div>
         <div v-else-if="data">
-          {{ data }}
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Birdthday</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="driver in data.listDrivers.items" :key="driver.id">
+                <td>{{ driver.name }}</td>
+                <td>{{ driver.birdthday }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </template>
     </amplify-connect>
@@ -52,6 +37,7 @@ const listDriversQuery = `
   query listDrivers {
   listDrivers {
     items{
+      id
       name
       birdthday
     }
